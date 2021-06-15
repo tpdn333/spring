e0d0be437e595c94a5d96c474417774828ea9619
tpdn333 <tpdn333@gmail.com>
2021-06-15 11:37:25 +0900

    test

--- src/main/java/lombok/test/User.java
+++ src/main/java/lombok/test/User.java
@@ -1,11 +1,12 @@
 package lombok.test;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@ToString
-@RequiredArgsConstructor
+
 // final 필드만 생성자 생성
+@Data
 public class User {
 	private final String name;
 	private double price;
