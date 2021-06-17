0e669d6548f318c8982ee04649eca04007447c1f
tpdn333 <tpdn333@gmail.com>
2021-06-17 15:47:30 +0900

    samplecontroller

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,74 @@
+package org.zerock.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.controller.lecture.domain.TodoDTO;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+	
+	@InitBinder // 먼저 실행되는 메소드
+	public void initBinder(WebDataBinder binder) {
+		log.info("내가 1등");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
+	@RequestMapping("")
+	public void basic() {
+		log.info("basic.....................");
+	}
+
+	@RequestMapping(value = "/basic", method = { RequestMethod.GET, RequestMethod.POST })
+	public void basicGet() {
+		log.info("basic get......................");
+	}
+
+	@RequestMapping("/basicOnlyGet")
+	public void basicGet2() {
+		log.info("basic get Only get...................");
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name,
+						@RequestParam("age") int age) {
+		log.info("name: " + name);
+		log.info("age : " + age);
+		
+		return "ex02";
+	}
+	
+	@GetMapping("ex02List")
+	public String ex02List(@RequestParam("ids") ArrayList<String> ids) {
+		log.info("ids : " + ids);
+		return "ex02List";
+	}
+	
+	@GetMapping("ex02Array")
+	public String ex02Array(@RequestParam("ids") String[] ids) {
+		log.info("array ids : " + Arrays.toString(ids));
+		return "ex02Array";
+	}
+	
+	@GetMapping("ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo : " + todo);
+		return "ex03";
+	}
+	
+}
