362d638f9619bbc2c614ce41fea22fdb1e1aa79d
tpdn333 <tpdn333@gmail.com>
2021-06-21 17:37:38 +0900

    sql file

---
+++ src/main/webapp/WEB-INF/sql/SQL01.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE spr1;
+USE spr1;
+
+CREATE TABLE tbl_board(
+	bno INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    content VARCHAR(2000) NOT NULL,
+    writer VARCHAR(50) NOT NULL,
+    regdate TIMESTAMP DEFAULT NOW(),
+    updatedate TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO tbl_board (title, content, writer)
+VALUES ('testTitle', 'test content', 'user5');
+
+SELECT * FROM tbl_board;
+
+-- 연습
+CREATE TABLE tbl_test1 (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL
+);
+
+INSERT INTO tbl_test1 (name, age) VALUES ('jimin', 22);
+INSERT INTO tbl_test1 (name, age) VALUES ('suga', 33);
+INSERT INTO tbl_test1 (name, age) VALUES ('dbfla', 66);
+
+SELECT * FROM tbl_test1;
+
+-- org.zerock.domain.Test1VO (bean)
+-- org.zerock.mapper.Test1Mapper (interface)
+-- org.zerock.mapper.Test1Mapper.xml (xml)
+-- Test1Tests
