15899ba8946c7479460b69ba210b2ccac42a4e6a
tpdn333 <tpdn333@gmail.com>
2021-06-25 17:57:29 +0900

    domain

--- src/main/java/org/zerock/domain/Criteria.java
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -12,6 +12,9 @@ public class Criteria {
 	private int pageNum;
 	private int amount;
 	
+	private String type;
+	private String keyword;
+	
 	public Criteria() {
 		this(1, 10);
 	}
@@ -23,6 +26,15 @@ public class Criteria {
 	
 	public int getFrom() {
 		return amount * (pageNum - 1);
+	}
+	
+	public String[] getTypeArr() {
+		if(type == null) {
+			return new String[] {};
+		} else {
+			String[] types = type.split("");
+			return types;
+		}
 	}
 	
 }
