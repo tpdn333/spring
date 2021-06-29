3d28f975232e8f2322c094c25e544cf427883b4a
tpdn333 <tpdn333@gmail.com>
2021-06-29 16:55:43 +0900

    sql file

---
+++ src/main/webapp/WEB-INF/sql/SQL04.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tbl_reply(
+	rno INT PRIMARY KEY AUTO_INCREMENT,
+	bno INT NOT NULL,
+    reply VARCHAR(2000) NOT NULL,
+    replyer VARCHAR(50) NOT NULL,
+    replyDate TIMESTAMP DEFAULT NOW(),
+    updatedate TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (bno) REFERENCES tbl_board(bno)
+);
+
+ALTER TABLE tbl_reply ADD CONSTRAINT pk_relpy PRIMARY KEY (rno); -- X
+ALTER TABLE tbl_reply ADD CONSTRAINT fk_reply_board FOREIGN KEY (bno) REFERENCES tbl_board(bno);
+
+SELECT * FROM tbl_board;
+
+SELECT * FROM tbl_reply;
