93c2c95d6474feb1301bcf03a1f87dde313990cf
tpdn333 <tpdn333@gmail.com>
2021-07-07 16:56:09 +0900

    security

--- src/main/java/org/zerock/security/domain/CustomUser.java
+++ src/main/java/org/zerock/security/domain/CustomUser.java
@@ -1,17 +1,30 @@
 package org.zerock.security.domain;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.zerock.domain.MemberVO;
 
+import lombok.Getter;
+
+// 책 668 쪽
 public class CustomUser extends User{
 
+	@Getter
+	private MemberVO member; 
+	
 	public CustomUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
 		super(username, password, authorities);
 		// TODO Auto-generated constructor stub
 	}
 
-
+	public CustomUser(MemberVO vo) {
+		super(vo.getUserid(), vo.getUserpw(), vo.getAuthList().stream()
+				.map(auth -> new SimpleGrantedAuthority(auth.getAuth()))
+				.collect(Collectors.toList()));
+		member = vo;
+	}
 }
