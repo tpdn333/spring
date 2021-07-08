aea570394228d55ba7fefcb75a96cb481ed73d4a
tpdn333 <tpdn333@gmail.com>
2021-07-08 18:25:44 +0900

    service

---
+++ src/main/java/org/zerock/service/MemberService.java
@@ -0,0 +1,15 @@
+package org.zerock.service;
+
+import org.zerock.domain.MemberVO;
+
+public interface MemberService {
+
+	public boolean insert(MemberVO vo);
+
+	public MemberVO read(String name);
+
+	public boolean modify(MemberVO vo);
+
+	public boolean remove(MemberVO vo);
+}
+
